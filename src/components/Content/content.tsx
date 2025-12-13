631fb3b4020354468570ec3629b000d7918b20f5
valdemar-leontev <vladimi.leontev@x5.ru>
2025-12-13 14:50:56 +0300

    fix build

--- src/components/Content/content.tsx
+++ src/components/Content/content.tsx
@@ -3,8 +3,8 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import { Cart } from '@/pages/Cart/cart'
 import { Home } from '@/pages/Home/home'
 import { motion, AnimatePresence } from 'framer-motion'
-import { Catalog } from '@/pages/Catalog/catalog'
-import { Favorites } from '@/pages/Favorite/favorite'
+import { Catalog } from '../../pages/Catalog/catalog'
+import { Favorites } from '../../pages/Favorite/favorite'
 
 
 export const Content = () => {
